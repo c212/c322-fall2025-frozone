ba0345e4f4e99bf4d1dcb762122fd84b2660bfe5
c212 <40395648+c212@users.noreply.github.com>
2025-11-19 00:53:03 -0500

    stage03

---
+++ Controller.java
@@ -0,0 +1,19 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Controller implements KeyListener {
+  private Model m;
+  private View v;
+  public Controller(Model m, View v) {
+    this.m = m;
+    this.v = v;
+    System.out.println("Controller for " + v.id + " created."); 
+  }
+  public void keyPressed(KeyEvent e) {
+    System.out.println("Controller for " + v.id + " " + e);
+  }
+  public void keyReleased(KeyEvent e) { }
+  public void keyTyped(KeyEvent e) { }
+}
+
+// ---( Controller.java )---
--- Example.java
+++ Example.java
@@ -6,11 +6,11 @@ public class Example {
     for (int i = 0; i < args.length; i++) {
       System.out.println("Processing " + args[i]);
       View v = new View(m, i); 
-
+      Controller c = new Controller(m, v);
       JFrame f = new JFrame("Player: " + args[i]); 
       f.setSize(400, 400); 
       f.add(v);
-      
+      f.addKeyListener(c);
       f.setVisible(true);      
     }
   }
